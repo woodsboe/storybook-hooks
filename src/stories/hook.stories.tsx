66fd529fc97ef2cff776b8c60a7cd7239bd8c5af
Nikolaj Schouboe Lovring <nschouboe@gmail.com>
2023-10-06 09:51:55 +0200

    Reformatting code

--- src/stories/hook.stories.tsx
+++ src/stories/hook.stories.tsx
@@ -1,28 +1,27 @@
-import {Meta, StoryObj} from "@storybook/react";
-import {GenerateGuidProps, useGenerateGuid} from "./hooks/useGenerateGuid";
-import React from 'react';
+import { Meta, StoryObj } from "@storybook/react";
+import { GenerateGuidProps, useGenerateGuid } from "./hooks/useGenerateGuid";
+import React from "react";
 
 const HookStory = (args: GenerateGuidProps) => {
-
-  return (<HookStoryWithProps {...args}/>)
+  return <HookStoryWithProps {...args} />;
 };
 
 const HookStoryWithProps = (args: GenerateGuidProps) => {
   const guid = useGenerateGuid(args);
-  return (<>{guid}</>)
-}
+  return <>{guid}</>;
+};
+
 const meta = {
-  title: 'useGenerateGuid',
+  title: "useGenerateGuid",
   component: HookStory,
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   args: {
-    input: 'Hook documentation'
+    input: "Hook documentation",
   },
-} satisfies Meta<typeof HookStory>
+} satisfies Meta<typeof HookStory>;
 
 export default meta;
 
 type Story = StoryObj<typeof HookStory>;
 
-export const Default: Story = {
-}
+export const Default: Story = {};
